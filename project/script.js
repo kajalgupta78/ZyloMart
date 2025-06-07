e5aee7c280d10840e2ac9744ef0a32c1419f289b
kajalgupta78 <kajallgupta91@gmail.com>
2025-06-06 22:47:10 -0700

    Delete project directory

--- project/script.js
+++
@@ -1,9 +0,0 @@
-const list = document.querySelector(".navlist");
-const hamburger = document.querySelector(".fa-bars");
-
-
-hamburger.addEventListener("click",()=>{
-
-   hamburger.classList.toggle("fa-x");
-    list.classList.toggle("navlist-active");
-})
